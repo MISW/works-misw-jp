f061220a72b1043442e0a0ceb5b9a23202013333
CaffeeLake <PascalCoffeeLake@gmail.com>
2026-06-13 18:51:08 +0900

    Some fixes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-!(function () {
+(!(function () {
   for (var e = document.querySelectorAll('a'), t = 0; t < e.length; t++) e[t].host !== location.host && e[t].setAttribute('target', '_blank');
 })(),
   (function () {
@@ -6,12 +6,12 @@
       for (i = 0; i < o.length; i++)
         if (e.matches) {
           var t = document.createElement('div');
-          t.classList.add('video-wrap'), o[i].parentNode.insertBefore(t, o[i]), t.appendChild(o[i]);
+          (t.classList.add('video-wrap'), o[i].parentNode.insertBefore(t, o[i]), t.appendChild(o[i]));
         } else t.classList.remove('video-wrap');
     }
     var t = matchMedia('(max-width: 1080px)'),
       o = document.querySelectorAll('.post .entry-content iframe[src*="youtube.com/embed"]');
-    e(t), t.addListener(e);
+    (e(t), t.addListener(e));
   })(),
   (function () {
     for (var e = document.querySelectorAll('.index-post.text'), t = 0; t < e.length; t++)
@@ -20,9 +20,9 @@
         if (o) {
           var r = e[t].querySelector('.post-content-anchor'),
             n = o.getAttribute('src');
-          r.insertAdjacentHTML('afterbegin', "<div class='post-photo-thumb' style='background-image: url(" + n + "');'></div>"),
+          (r.insertAdjacentHTML('afterbegin', "<div class='post-photo-thumb' style='background-image: url(" + n + "');'></div>"),
             r.classList.remove('post-content-anchor'),
-            (e[t].querySelector('.post-content-wrapper').style.display = 'none');
+            (e[t].querySelector('.post-content-wrapper').style.display = 'none'));
         }
       }
-  })();
+  })());
